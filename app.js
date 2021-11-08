056d9c0fceb655427ced20fcf6af84cfd41fe930
Rickard <73134024+RickardVMiun@users.noreply.github.com>
2021-11-08 15:39:29 +0100

    CSS-ändringar
    Ändrat hur information visas, formaterat om CSS.

--- app.js
+++ app.js
@@ -1,142 +1,176 @@
 const weekfood = [
 {
     "dish": "Korv och makaroner",
-    "image": "https://i.imgur.com/5lyTCoV.jpg"
+    "image": "https://i.imgur.com/5lyTCoV.jpg",
+    "ingredienter": "https://www.mathem.se/recept/korv-och-makaroner"
 },
 {
     "dish": "Spaghetti och köttfärsås",
-    "image": "https://i.imgur.com/5MnJ3iE.jpg"
+    "image": "https://i.imgur.com/5MnJ3iE.jpg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 },
 {
     "dish": "Gulashsoppa",
-    "image": "https://i.imgur.com/i8QgWwr.jpg"
+    "image": "https://i.imgur.com/i8QgWwr.jpg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 },
 {
     "dish": "Lasagne",
-    "image": "https://i.imgur.com/9ukynfp.jpg"
+    "image": "https://i.imgur.com/9ukynfp.jpg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 },
 {
     "dish": "Korvstroganoff",
-    "image": "https://i.imgur.com/uUxwXWK.jpg"
+    "image": "https://i.imgur.com/uUxwXWK.jpg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 },
 {
     "dish": "Baconpasta",
-    "image": "https://i.imgur.com/6HghI5O.jpg"
+    "image": "https://i.imgur.com/6HghI5O.jpg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 },
 {
     "dish": "Lax med potatis",
-    "image": "https://i.imgur.com/BgRZnx2.jpg"
+    "image": "https://i.imgur.com/BgRZnx2.jpg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 },
 {
     "dish": "Köttbullar med makaroner",
-    "image": "https://i.imgur.com/JfwiTIa.jpg"
+    "image": "https://i.imgur.com/JfwiTIa.jpg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 },
 {
     "dish": "Tacos",
-    "image": "https://i.imgur.com/AKYyxKG.jpg"
+    "image": "https://i.imgur.com/AKYyxKG.jpg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 },
 {
     "dish": "Korv med stuvade makaroner",
-    "image": "https://i.imgur.com/5lyTCoV.jpg"
+    "image": "https://i.imgur.com/5lyTCoV.jpg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 },
 {
     "dish": "Potatisbullar",
-    "image": "https://i.imgur.com/9zaViHm.jpg"
+    "image": "https://i.imgur.com/9zaViHm.jpg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 },
 {
     "dish": "Krämig carbonara",
-    "image": "https://i.imgur.com/aghtRXK.jpg"
+    "image": "https://i.imgur.com/aghtRXK.jpg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 },
 {
     "dish": "Raggmunk med fläsk",
-    "image": "https://i.imgur.com/FFQVYXY.jpg"
+    "image": "https://i.imgur.com/FFQVYXY.jpg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 },
 {
     "dish": "Flygande Jacob",
-    "image": "https://i.imgur.com/WbDqrzf.jpeg"
+    "image": "https://i.imgur.com/WbDqrzf.jpeg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 },
 {
     "dish": "Fläskpannkaka",
-    "image": "https://i.imgur.com/hJ7XmeP.jpg"
+    "image": "https://i.imgur.com/hJ7XmeP.jpg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 },
 {
     "dish": "Baconlindad kycklingfilé i ugn",
-    "image": "https://i.imgur.com/SLIH4MT.jpg"
+    "image": "https://i.imgur.com/SLIH4MT.jpg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 },
 {
     "dish": "Köttfärsbiffar med gräddsås",
-    "image": "https://i.imgur.com/TEPcdwc.jpg"
+    "image": "https://i.imgur.com/TEPcdwc.jpg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 },
 {
     "dish": "Makaronipudding med falukorv",
-    "image": "https://i.imgur.com/vbW8THN.jpg"
+    "image": "https://i.imgur.com/vbW8THN.jpg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 },
 {
     "dish": "Klassisk makaronipudding",
-    "image": "https://i.imgur.com/vbW8THN.jpg"
+    "image": "https://i.imgur.com/vbW8THN.jpg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 },
 {
     "dish": "Kyckling i paprikasås",
-    "image": "https://i.imgur.com/xbfy9fq.jpg"
+    "image": "https://i.imgur.com/xbfy9fq.jpg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 },
 {
     "dish": "Pannkaka",
-    "image": "https://i.imgur.com/08Tg5Q8.jpg"
+    "image": "https://i.imgur.com/08Tg5Q8.jpg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 },
 {
     "dish": "Kasslergratäng",
-    "image": "https://i.imgur.com/B2UwFnb.jpg"
+    "image": "https://i.imgur.com/B2UwFnb.jpg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 },
 {
     "dish": "Hamburgare",
-    "image": "https://i.imgur.com/DZKAvOn.jpg"
+    "image": "https://i.imgur.com/DZKAvOn.jpg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 },
 {
     "dish": "Broccolisoppa",
-    "image": "https://i.imgur.com/QFgcp8D.jpg"
+    "image": "https://i.imgur.com/QFgcp8D.jpg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 },
 {
     "dish": "Hemgjord pizza",
-    "image": "https://i.imgur.com/lKodFX0.jpg"
+    "image": "https://i.imgur.com/lKodFX0.jpg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 },
 {
     "dish": "Omelett med ost och skinka",
-    "image": "https://i.imgur.com/I7czfFY.jpg"
+    "image": "https://i.imgur.com/I7czfFY.jpg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 },
 {
     "dish": "Köttfärsbiffar i timjansås",
-    "image": "https://i.imgur.com/gCwRpi2.jpg"
+    "image": "https://i.imgur.com/gCwRpi2.jpg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 },
 {
     "dish": "Crepes med räkfyllning",
-    "image": "https://i.imgur.com/yGetYmt.jpg"
+    "image": "https://i.imgur.com/yGetYmt.jpg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 },
 {
     "dish": "Chili con carne",
-    "image": "https://i.imgur.com/2OhGWh4.jpg"
+    "image": "https://i.imgur.com/2OhGWh4.jpg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 }
 ]
 
 const weekendfood = [
 {
     "dish": "Fläskfilé med potatis",
-    "image": "https://i.imgur.com/IV9Dzah.jpg"
+    "image": "https://i.imgur.com/IV9Dzah.jpg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 },
 {
     "dish": "Kyckling med mögelostsås",
-    "image": "https://i.imgur.com/QMyHvJM.jpeg"
+    "image": "https://i.imgur.com/QMyHvJM.jpeg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 },
 {
     "dish": "Biffar med ris och tzatziki",
-    "image": "https://i.imgur.com/P7pVEwu.jpg"
+    "image": "https://i.imgur.com/P7pVEwu.jpg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 },
 {
     "dish": "Köttfärslimpa med potatis",
-    "image": "https://i.imgur.com/NCyeUHr.jpg"
+    "image": "https://i.imgur.com/NCyeUHr.jpg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 },
 {
     "dish": "Fläskfilégryta med champinjoner",
-    "image": "https://i.imgur.com/NSdEjkm.jpg"
+    "image": "https://i.imgur.com/NSdEjkm.jpg",
+    "ingredienter": "https://www.ica.se/recept/spaghetti-och-kottfarssas-712805/"
 }
 ]
 
@@ -166,24 +200,31 @@ while(weekendarr.length < 2){
 console.log(weekendarr);
     document.querySelector('#mondaydish').innerHTML = weekfood[weekarr[0]].dish;
     document.querySelector('#mondayimage').src = weekfood[weekarr[0]].image;
+    document.querySelector('#mondayingredient').href = weekfood[weekarr[0]].ingredienter;
 
     document.querySelector('#tuesdaydish').innerHTML = weekfood[weekarr[1]].dish;
     document.querySelector('#tuesdayimage').src = weekfood[weekarr[1]].image
+    document.querySelector('#tuesdayingredient').href = weekfood[weekarr[1]].ingredienter;
 
     document.querySelector('#wednesdaydish').innerHTML = weekfood[weekarr[2]].dish;
     document.querySelector('#wednesdayimage').src = weekfood[weekarr[2]].image
+    document.querySelector('#wednesdayingredient').href = weekfood[weekarr[2]].ingredienter;
 
     document.querySelector('#thursdaydish').innerHTML = weekfood[weekarr[3]].dish;
     document.querySelector('#thursdayimage').src = weekfood[weekarr[3]].image
+    document.querySelector('#thursdayingredient').href = weekfood[weekarr[3]].ingredienter;
 
     document.querySelector('#fridaydish').innerHTML = weekfood[weekarr[4]].dish;
     document.querySelector('#fridayimage').src = weekfood[weekarr[4]].image
+    document.querySelector('#fridayingredient').href = weekfood[weekarr[4]].ingredienter;
 
     document.querySelector('#saturdaydish').innerHTML = weekendfood[weekendarr[0]].dish;
     document.querySelector('#saturdayimage').src = weekendfood[weekendarr[0]].image
+    document.querySelector('#saturdayingredient').href = weekendfood[weekendarr[0]].ingredienter;
 
     document.querySelector('#sundaydish').innerHTML = weekendfood[weekendarr[1]].dish;
     document.querySelector('#sundayimage').src = weekendfood[weekendarr[1]].image
+    document.querySelector('#sundayingredient').href = weekendfood[weekendarr[1]].ingredienter;
   }
 
   console.log("js found");
